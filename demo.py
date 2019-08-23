43d72a24a65a521b777643058c9d6f690059a3a5
Richard Pappalardo <rpappalax@gmail.com>
2019-08-23 15:26:07 -0700

    working demo

--- demo.py
+++ demo.py
@@ -1,11 +1,13 @@
 import os
 from github import Github
 
+
 GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
+
 # First create a Github instance:
-# or using an access token
+# using an access token
 g = Github(GITHUB_TOKEN)
 
-# Then play with your Github objects:
-for repo in g.get_user().get_repos():
-    print(repo.name)
+repo = g.get_repo('bravenewrichard/demo-pr-comment')
+pr = repo.get_pull(1)
+pr.create_issue_comment('test')
